9a890deb0bdeb04fda971fcc2a7136e6a185720c
KJam <kaytchjam@gmail.com>
2021-03-20 21:36:59 -0500

    Update script.js
    Rearranged script.js

--- script.js
+++ script.js
@@ -14,96 +14,6 @@ centerY = canvas.height / 2;
 
 //Drawing & Manipulating Canvas ***
 
-/*
-context.fillStyle = "black";
-
-context.fillRect(20, 20, 150, 100);
-context.strokeRect(20, 140, 150, 100);
-
-context.font = "30px Helvetica";
-context.fillText("Hello world", 200, 40);*/
-
-//Paths in Canvas ***
-/*
-context.beginPath();
-context.moveTo(50, 50);
-context.lineTo(150, 50);
-context.lineTo(100, 200);
-context.lineTo(50, 50);
-context.fillStyle = "purple";
-context.fill();
-//context.closePath path goes back to beginning
-//context.stroke();
-
-context.beginPath();
-context.moveTo(200, 50);
-context.lineTo(150, 200);
-context.lineTo(250, 200);
-context.lineTo(200, 50);
-context.stroke();
-
-context.beginPath();
-context.moveTo(350, 50);
-context.lineTo(400, 200);
-context.lineTo(450, 200);
-context.lineTo(400, 50);
-context.lineTo(350, 50);
-context.fillStyle = "red";
-context.fill();*/
-
-// Circles Smiley Face
-/*
-context.beginPath();
-context.arc(centerX, centerY, 100, 0, Math.PI * 2);
-context.moveTo(centerX + 50, centerY);
-context.arc(centerX, centerY, 50, 0, Math.PI, false);
-context.moveTo(centerX-30, centerY-40);
-context.arc(centerX-40, centerY-40, 10, 0, Math.PI * 2);
-context.moveTo(centerX+50, centerY-40);
-context.arc(centerX+40, centerY-40, 10, 0, Math.PI*2);
-context.stroke();*/
-
-/*
-const circle = {
-  x: 100,
-  y: 100,
-  size: 30,
-  dx: 2.5,
-  dy: 2
-};
-
-function drawCircle() {
-  context.beginPath();
-  context.arc(circle.x, circle.y, circle.size, 0, Math.PI *2);
-  context.fillStyle = "gray";
-  context.fill();
-}
-
-function update() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  drawCircle();
-
-  //change position
-  circle.x += circle.dx;
-  circle.y += circle.dy;
-
-  // Detect Side Walls
-  if (circle.x + circle.size > canvas.width || circle.x - circle.size < 0) {
-    console.log("HIT");
-    circle.dx *= -1;
-  }
-
-  // Detect Top & Bottom Walls
-  if (circle.y + circle.size > canvas.height || circle.y - circle.size < 0) {
-    console.log("HIT");
-    circle.dy *= -1;
-  }
-
-  requestAnimationFrame(update);
-}
-
-update();*/
-
 // Animation 2
 
 const image = document.getElementById('source');
@@ -208,3 +118,93 @@ update();
 
 document.addEventListener('keydown', keyDown);
 document.addEventListener('keyup', keyUp);
+
+/*
+context.fillStyle = "black";
+
+context.fillRect(20, 20, 150, 100);
+context.strokeRect(20, 140, 150, 100);
+
+context.font = "30px Helvetica";
+context.fillText("Hello world", 200, 40);*/
+
+//Paths in Canvas ***
+/*
+context.beginPath();
+context.moveTo(50, 50);
+context.lineTo(150, 50);
+context.lineTo(100, 200);
+context.lineTo(50, 50);
+context.fillStyle = "purple";
+context.fill();
+//context.closePath path goes back to beginning
+//context.stroke();
+
+context.beginPath();
+context.moveTo(200, 50);
+context.lineTo(150, 200);
+context.lineTo(250, 200);
+context.lineTo(200, 50);
+context.stroke();
+
+context.beginPath();
+context.moveTo(350, 50);
+context.lineTo(400, 200);
+context.lineTo(450, 200);
+context.lineTo(400, 50);
+context.lineTo(350, 50);
+context.fillStyle = "red";
+context.fill();*/
+
+// Circles Smiley Face
+/*
+context.beginPath();
+context.arc(centerX, centerY, 100, 0, Math.PI * 2);
+context.moveTo(centerX + 50, centerY);
+context.arc(centerX, centerY, 50, 0, Math.PI, false);
+context.moveTo(centerX-30, centerY-40);
+context.arc(centerX-40, centerY-40, 10, 0, Math.PI * 2);
+context.moveTo(centerX+50, centerY-40);
+context.arc(centerX+40, centerY-40, 10, 0, Math.PI*2);
+context.stroke();*/
+
+/*
+const circle = {
+  x: 100,
+  y: 100,
+  size: 30,
+  dx: 2.5,
+  dy: 2
+};
+
+function drawCircle() {
+  context.beginPath();
+  context.arc(circle.x, circle.y, circle.size, 0, Math.PI *2);
+  context.fillStyle = "gray";
+  context.fill();
+}
+
+function update() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  drawCircle();
+
+  //change position
+  circle.x += circle.dx;
+  circle.y += circle.dy;
+
+  // Detect Side Walls
+  if (circle.x + circle.size > canvas.width || circle.x - circle.size < 0) {
+    console.log("HIT");
+    circle.dx *= -1;
+  }
+
+  // Detect Top & Bottom Walls
+  if (circle.y + circle.size > canvas.height || circle.y - circle.size < 0) {
+    console.log("HIT");
+    circle.dy *= -1;
+  }
+
+  requestAnimationFrame(update);
+}
+
+update();*/
